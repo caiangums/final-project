dd3fafdd6c2d0eaad67172a2f047c0d3dddb9794
Ramna Sidharta <ramna.thors@gmail.com>
2019-10-25 21:48:59 -0300

    Remove comments of dirp.cc to increase legibility

--- src/network/ipv4/dirp.cc
+++ src/network/ipv4/dirp.cc
@@ -13,18 +13,9 @@ DIRP* DIRP::_networks[];
 
 // TODOr use abstractions like Packet and Header
 int DIRP::send(const Address::Local & from, const Address & to, const void * data, unsigned int size) {
-    //NIC<Ethernet> * nic = Traits<Ethernet>::DEVICES::Get<0>::Result::get(0);
-    //return nic->send(nic->broadcast(), Ethernet::PROTO_DIR, data, size);
-
     // Get singleton DIRP
     DIRP * dirp = DIRP::get_by_nic(0);
     
-    // Get buffer allocated by NIC
-    //Buffer* pool = dirp->_nic->alloc(to.mac(), DIRP::PROTOCOL, sizeof(Header), sizeof(Header), size);
-
-    //Packet* packet = new Packet(from, to.port(), size);
-    //memcpy(packet->data<void>(), data, size);
-
     // our packet is the data to be sent plus the destin port (an unsigned int, 4 bytes) in the front
     char packet[size + 4];
     unsigned int port = to.port();
@@ -42,19 +33,6 @@ int DIRP::receive(Buffer * buf, void * d, unsigned int s) {
     memcpy(d, &(data[4]), s);  // 4 bytes is the port length (unsigned int)
     buf->nic()->free(buf);
     return s;
-
-    /*
-    unsigned char * data = reinterpret_cast<unsigned char *>(d);
-    Packet* packet = buf->frame()->data<Packet>();
-
-    packet = buf->frame()->data<Packet>();
-    unsigned int len = buf->size() - sizeof(Header);
-    memcpy(data, packet->data<void>(), len);
-
-    buf->nic()->free(buf);
-
-    return buf->size();
-    */
 }
 
 // TODOr use abstractions like Packet and Header
